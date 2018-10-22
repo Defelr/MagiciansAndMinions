dc80a9194437cac6f701025fed2ead0e6e6b9bde
Robert DeFelice <trombonedrummer@gmail.com>
2018-10-22 11:32:26 -0400

    Initial commit
    Added both the old and new projects to gitHub

---
+++ Magicians and Minions Revised/Assets/Scripts/User Interface/MainMenu.cs
@@ -0,0 +1,71 @@
+﻿/* 
+This script holds functions for the main menu including:
+   - Loading Scenes
+   - Displaying Panels
+   - Exiting the game
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+
+    /* Variables */
+
+    //UI Panels
+    public GameObject titlePanel;
+    public GameObject mainMenuPanel;
+    public GameObject helpPanel;
+    public GameObject creditsPanel;
+    
+
+    /* Fuctions */
+    
+    public void HelpPanel() //shows or hides the help panel
+    {
+        bool active = helpPanel.activeInHierarchy;
+        if(active == false)
+        {
+            titlePanel.SetActive(false);
+            mainMenuPanel.SetActive(false);
+            helpPanel.SetActive(true);
+        }
+        else
+        {
+            helpPanel.SetActive(false);
+            titlePanel.SetActive(true);
+            mainMenuPanel.SetActive(true);
+        }
+    }
+
+    public void CreditsPanel() //shows or hides the credits panel
+    {
+        bool active = creditsPanel.activeInHierarchy;
+        if(active == false)
+        {
+            titlePanel.SetActive(false);
+            mainMenuPanel.SetActive(false);
+            creditsPanel.SetActive(true);
+        }
+        else
+        {
+            creditsPanel.SetActive(false);
+            titlePanel.SetActive(true);
+            mainMenuPanel.SetActive(true);
+        }
+    }
+    
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
